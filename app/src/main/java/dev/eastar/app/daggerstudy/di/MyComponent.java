5626d768f76171875ea92be5a67508dcf1c62441
eastar Jeong <438869+eastar-dev@users.noreply.github.com>
2020-06-03 21:13:02 +0900

    MembersInjector<T> 받아서 작업

--- app/src/main/java/dev/eastar/app/daggerstudy/di/MyComponent.java
+++ app/src/main/java/dev/eastar/app/daggerstudy/di/MyComponent.java
@@ -1,8 +1,11 @@
 package dev.eastar.app.daggerstudy.di;
 
 import dagger.Component;
+import dagger.MembersInjector;
 
 @Component(modules = {MyModuleA.class})
 public interface MyComponent {
     void inject(MyClass myClass);
+
+    MembersInjector<MyClass> getInjector();
 }
--- app/src/test/java/dev/eastar/app/daggerstudy/di/DaggerTest.java
+++ app/src/test/java/dev/eastar/app/daggerstudy/di/DaggerTest.java
@@ -2,6 +2,8 @@ package dev.eastar.app.daggerstudy.di;
 
 import org.junit.Test;
 
+import dagger.MembersInjector;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
@@ -13,6 +15,18 @@ public class DaggerTest {
 
         MyComponent myComponent = DaggerMyComponent.create();
         myComponent.inject(myClass);
+        assertEquals("eastar 10", myClass.name + " " + myClass.age);
+        assertEquals("eastar 10", myClass.name + " " + myClass.age2);
+    }
+
+    @Test
+    public void testMemberInjection2() {
+        MyClass myClass = new MyClass();
+        assertNull("처음에 그냥 받으면 null 이다", myClass.name);
+
+        MembersInjector<MyClass> injector = DaggerMyComponent.create().getInjector();
+        injector.injectMembers(myClass);
+
         assertEquals("eastar 10", myClass.name + " " + myClass.age);
         assertEquals("eastar 10", myClass.name + " " + myClass.age2);
     }
