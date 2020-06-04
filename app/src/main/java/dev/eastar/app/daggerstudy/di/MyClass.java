15179fe9a474d0781d2b2d8e265f9938b3a4ebc2
eastar Jeong <438869+eastar-dev@users.noreply.github.com>
2020-06-05 01:19:33 +0900

    @Inject에 한정자를 붙여서 받으면 @Named("first") 붙어 있는 놈이 온다.

--- app/src/main/java/dev/eastar/app/daggerstudy/di/MyClass.java
+++ app/src/main/java/dev/eastar/app/daggerstudy/di/MyClass.java
@@ -1,9 +1,11 @@
 package dev.eastar.app.daggerstudy.di;
 
 import javax.inject.Inject;
+import javax.inject.Named;
 
 public class MyClass {
     @Inject
+    @Named("first")
     String str;
 
     public String getStr() {
